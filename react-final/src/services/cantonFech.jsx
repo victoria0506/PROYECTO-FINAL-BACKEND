8fb396aaaf6d6e6366e748ff77176bf06745c8d8
gary2504 <garychaves25@gmail.com>
2024-10-17 17:23:59 -0600

    Avance pagina

--- react-final/src/services/cantonFech.jsx
+++ react-final/src/services/cantonFech.jsx
@@ -1,5 +1,5 @@
-// const Token= "d58379a2f75349bbb55f641fd6c323527b1f495a"
-const Token= "07881b7aeb97068cd9925d768fd3af4b77cb7eab"
+const Token= "d58379a2f75349bbb55f641fd6c323527b1f495a"
+//const Token= "07881b7aeb97068cd9925d768fd3af4b77cb7eab"
 const canton2 = async () => {
     try {
         const response = await fetch('http://localhost:8000/api/canton/', {
